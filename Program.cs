1329e4483237bc26666dc0dcbdb11f46343eda1d
blakemoore-dev <lostinthestack@protonmail.com>
2021-10-11 10:57:42 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAccount01 = new BankAccount();
+            Console.WriteLine($"Your current blance is:    {bankAccount01.GetBalance()}");
+            Console.Write("\nHow much would you like to deposit?    ");
+            bankAccount01.Deposit(double.Parse(Console.ReadLine()));
+            Console.WriteLine($"\nYour current balnce is {bankAccount01.GetBalance()}");
+        }
+    }
+}
